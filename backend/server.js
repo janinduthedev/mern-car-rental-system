92fd34581ff8d1b0b29eff9785f58b45fa117a33
Janindu Tharinda <janindu.the.dev@gmail.com>
2026-01-01 17:40:47 +0530

    Add user authentication with JWT and admin role

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./config/db.js";
 
+import authRoutes from "./routes/authRoutes.js";
+
 dotenv.config();
 connectDB();
 
@@ -22,3 +24,5 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+app.use("/api/auth", authRoutes);
