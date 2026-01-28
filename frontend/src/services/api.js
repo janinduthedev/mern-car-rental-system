8e41ec9a046a482afbf80c689470d290ec7e5000
Janindu Tharinda <janindu.the.dev@gmail.com>
2026-01-28 14:14:08 +0530

    Update env frontend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  // Render එකේ variable එකක් තියෙනවා නම් ඒක ගනී, නැත්නම් localhost එක ගනී.
+  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api",
 });
 
 export default api;
